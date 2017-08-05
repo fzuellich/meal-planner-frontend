92ff4a40317cc25b37ed0e00bfe07d86e918bd1a
fzuellich <fzuellich@gmail.com>
2017-08-05 13:57:24 +0200

    init

---
+++ lib/app_component.dart
@@ -0,0 +1,19 @@
+// Copyright (c) 2017, ferenc. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/angular2.dart';
+import 'package:angular_components/angular_components.dart';
+
+// AngularDart info: https://webdev.dartlang.org/angular
+// Components info: https://webdev.dartlang.org/components
+
+@Component(
+  selector: 'my-app',
+  styleUrls: const ['app_component.css'],
+  templateUrl: 'app_component.html',
+  directives: const [materialDirectives],
+  providers: const [materialProviders],
+)
+class AppComponent {
+  final String name = 'World';
+}
